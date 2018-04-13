e1a529a1a4468b7c99b4169281d4cd7d02489ca4
messense <messense@icloud.com>
2018-04-13 22:01:58 +0800

    Impl Division::get()

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,27 @@ mod data;
 
 use data::DIVISIONS;
 
+const CURRENT_REVISION: &str = "201607";
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct Division<'a> {
-    pub code: &'a str,
-    pub name: &'a str,
-    pub revision: &'a str,
+pub struct Division {
+    pub code: &'static str,
+    pub name: &'static str,
+    pub revision: &'static str,
 }
 
-impl<'a> Division<'a> {
+impl Division {
+
+    pub fn get(code: &str) -> Option<Self> {
+        let current_data = DIVISIONS[CURRENT_REVISION];
+        current_data.get_entry(code).map(|(key, name)| {
+            Division {
+                code: key,
+                name: name,
+                revision: CURRENT_REVISION,
+            }
+        })
+    }
 
     pub fn revisions() -> Vec<&'static str> {
         DIVISIONS.keys().cloned().collect()
@@ -20,8 +33,13 @@ impl<'a> Division<'a> {
 
 #[cfg(test)]
 mod tests {
+    use super::Division;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_get() {
+        let division = Division::get("110000").unwrap();
+        assert_eq!(division.code, "110000");
+        assert_eq!(division.name, "北京市");
+        assert_eq!(division.revision, "201607");
     }
 }
