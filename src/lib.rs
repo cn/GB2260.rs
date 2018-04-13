c877c13295aac276302890dd984138e24ad8bda3
messense <messense@icloud.com>
2018-04-13 22:12:18 +0800

    Impl Division::is_province()

--- src/lib.rs
+++ src/lib.rs
@@ -16,18 +16,32 @@ pub struct Division {
 impl Division {
 
     pub fn get(code: &str) -> Option<Self> {
-        let current_data = DIVISIONS[CURRENT_REVISION];
-        current_data.get_entry(code).map(|(key, name)| {
-            Division {
-                code: key,
-                name: name,
-                revision: CURRENT_REVISION,
-            }
+        Self::get_by_revision(code, CURRENT_REVISION)
+    }
+
+    pub fn get_by_revision(code: &str, revision: &str) -> Option<Self> {
+        DIVISIONS.get_entry(revision).and_then(|(rev, data)| {
+            data.get_entry(code).map(|(key, name)| {
+                Division {
+                    code: key,
+                    name: name,
+                    revision: rev,
+                }
+            })
         })
     }
 
     pub fn revisions() -> Vec<&'static str> {
         DIVISIONS.keys().cloned().collect()
+    }
+
+    pub fn province(&self) -> Self {
+        let code = format!("{}0000", &self.code[..2]);
+        Self::get_by_revision(&code, self.revision).unwrap()
+    }
+
+    pub fn is_province(&self) -> bool {
+        *self == self.province()
     }
 }
 
@@ -41,5 +55,6 @@ mod tests {
         assert_eq!(division.code, "110000");
         assert_eq!(division.name, "北京市");
         assert_eq!(division.revision, "201607");
+        assert!(division.is_province());
     }
 }
