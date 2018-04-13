447ed5f4e7169898cfc74f9699760f7a1cec9f8f
messense <messense@icloud.com>
2018-04-13 21:48:11 +0800

    impl Division::revisions()

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+extern crate phf;
+
+mod data;
+
+use data::DIVISIONS;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Division<'a> {
+    pub code: &'a str,
+    pub name: &'a str,
+    pub revision: &'a str,
+}
+
+impl<'a> Division<'a> {
+
+    pub fn revisions() -> Vec<&'static str> {
+        DIVISIONS.keys().cloned().collect()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
