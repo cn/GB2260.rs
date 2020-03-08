998b4295c82bd44cfff1ca1988ecb1d35412d143
messense <messense@icloud.com>
2020-03-08 21:53:34 +0800

    Bump phf to 0.8

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,9 +1,9 @@
 extern crate phf_codegen;
 
 use std::env;
+use std::fs;
+use std::io;
 use std::path::{Path, PathBuf};
-use std::fs::{self, File};
-use std::io::{self, BufRead, BufReader};
 
 fn generate_by_dir(dir: &Path) -> io::Result<Vec<String>> {
     let mut codes = Vec::new();
@@ -16,18 +16,20 @@ fn generate_by_dir(dir: &Path) -> io::Result<Vec<String>> {
         let code = path.file_stem().unwrap().to_string_lossy();
         codes.push(code.to_string());
         let code = code.replace("-", "_");
-        let file = File::open(&path)?;
-        let mut reader = BufReader::new(file);
-        print!("static DIVISIONS_{}: phf::Map<&'static str, &'static str> = ", code);
+        let bytes = fs::read(path).unwrap();
+        let content = String::from_utf8_lossy(&bytes);
+        print!(
+            "static DIVISIONS_{}: phf::Map<&'static str, &'static str> = ",
+            code
+        );
         let mut map = phf_codegen::Map::new();
-        for line in reader.lines().skip(1) {
-            let line = line?;
+        for line in content.lines().skip(1) {
             let parts: Vec<&str> = line.split('\t').collect();
             let code = parts[2];
             let division = parts[3];
-            map.entry(code.to_string(), &format!("\"{}\"", division));
+            map.entry(code, &format!("\"{}\"", division));
         }
-        map.build(&mut io::stdout()).unwrap();
+        print!("{}", map.build());
         println!(";\n");
     }
     Ok(codes)
@@ -39,8 +41,10 @@ fn main() {
     println!("#![allow(non_upper_case_globals)]\n");
     println!("use phf;\n");
     let path: PathBuf = data_dir.into();
-    let stats_codes = generate_by_dir(path.join("stats").as_path()).expect("generate from stats failed");
-    let contrib_codes = generate_by_dir(path.join("contrib").as_path()).expect("generate from contrib failed");
+    let stats_codes =
+        generate_by_dir(path.join("stats").as_path()).expect("generate from stats failed");
+    let contrib_codes =
+        generate_by_dir(path.join("contrib").as_path()).expect("generate from contrib failed");
     print!("pub static DIVISIONS: phf::Map<&'static str, &'static phf::Map<&'static str, &'static str>> = ");
     let mut map = phf_codegen::Map::new();
     for code in &stats_codes {
@@ -51,6 +55,6 @@ fn main() {
         let code_name = code.replace("-", "_");
         map.entry(&code[..], &format!("&DIVISIONS_{}", code_name));
     }
-    map.build(&mut io::stdout()).unwrap();
+    print!("{}", map.build());
     print!(";\n");
 }
